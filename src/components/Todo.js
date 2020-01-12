a95fc6dbfeee146b6ed989b65f0e1c44a6164252
Daniel Mattox <dmattox10@gmail.com>
2020-01-12 14:37:54 -0500

    Added Form for adding todos

---
+++ src/components/Todo.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class Todo extends Component {
+    constructor(props) {
+        super(props)
+        
+    }
+    render() {
+        const { todo } = this.props
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Todo
